58bcdac6d0031e5df54cc27b5584a87603d56905
Pavel Home <kapabahone@gmail.com>
2025-03-16 10:04:33 +0300

    begin. board 1st iteration

---
+++ Assets/Scripts/TokenStats.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class TokenStats : MonoBehaviour
+{
+    public int moveDistance = 1; // Сколько клеток за ход
+    public float moveSpeed = 1f; // Скорость анимации
+    public int health = 100;     // Пример дополнительной характеристики
+}
