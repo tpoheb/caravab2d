f00395280238fcddc6c04857cfd2e4fbd0bde5b1
Pavel Home <kapabahone@gmail.com>
2025-03-22 14:07:19 +0300

    инкапсуляция первая итерация. Готовы пути, нет реализации перехода

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class City : MonoBehaviour
+{
+    [SerializeField] private string cityName; // Название города
+    [SerializeField] private List<PathCellInitializer> inCityPaths = new List<PathCellInitializer>(); // Список путей в городе
+
+    // Свойства для доступа к данным
+    public string CityName => cityName;
+    public List<PathCellInitializer> Paths => inCityPaths;
+
+    void Start()
+    {
+        InitializeCity();
+    }
+
+    // Инициализация города
+    private void InitializeCity()
+    {
+        if (string.IsNullOrEmpty(cityName))
+        {
+            cityName = "Unnamed City";
+        }
+
+        // Инициализируем все пути в городе
+        foreach (var path in inCityPaths)
+        {
+            if (path != null)
+            {
+                path.InitializeCells();
+            }
+            else
+            {
+                Debug.LogWarning($"Обнаружен пустой путь в городе {cityName}");
+            }
+        }
+
+        Debug.Log($"Город {cityName} инициализирован. Всего путей: {inCityPaths.Count}");
+    }
+}
