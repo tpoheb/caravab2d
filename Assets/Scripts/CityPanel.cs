92bb11c5cbf64de53e79c758157bf0d638865a72
Pavel Home <kapabahone@gmail.com>
2025-03-29 12:55:40 +0300

    cost not different.  For playertoken

--- Assets/Scripts/CityPanel.cs
+++ Assets/Scripts/CityPanel.cs
@@ -1,52 +1,108 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class CityPanel : MonoBehaviour
 {
-    [Header("UI Elements")]
-    public GameObject panel;
-    public Text cityNameText;
-    public Button tradeButton;
-    //public Button closeButton;
+    [SerializeField] private City currentCity; // Ссылка на текущий город
+    [SerializeField] private GameObject pathButtonPrefab; // Префаб кнопки пути
+    [SerializeField] private Transform pathButtonsContainer; // Контейнер для кнопок путей
+    [SerializeField] private Button hireTeamButton; // Кнопка найма команды
+    [SerializeField] private Button buyGoodsButton; // Кнопка покупки товаров
+    [SerializeField] private float buttonSpacing = 10f; // Расстояние между кнопками
+    [SerializeField] private PlayerToken playerToken; // Ссылка на фишку игрока
+    [SerializeField] private TradeItemSystem tradeItemSystem;
 
-    [Header("Trade System")]
-    public TradeItemSystem tradeSystem;
+    private List<Button> pathButtons = new List<Button>(); // Список созданных кнопок путей
 
-    private CityData currentCity;
-
-    private void Start()
+    void Start()
     {
-        tradeButton.onClick.AddListener(OpenTrade);
-        //closeButton.onClick.AddListener(ClosePanel);
-        panel.SetActive(false);
+        InitializePanel();
+        gameObject.SetActive(true); // Панель изначально активна
     }
 
-    public void OpenPanel(CityData city)
+    public void InitializePanel()
+    {
+        if (currentCity == null)
+        {
+            Debug.LogError("Не задан текущий город для панели!");
+            return;
+        }
+
+        ClearPathButtons();
+
+        for (int i = 0; i < currentCity.Paths.Count; i++)
+        {
+            int pathIndex = i;
+            GameObject buttonObj = Instantiate(pathButtonPrefab, pathButtonsContainer);
+            Button pathButton = buttonObj.GetComponent<Button>();
+
+            RectTransform buttonRect = buttonObj.GetComponent<RectTransform>();
+            float buttonHeight = buttonRect.sizeDelta.y;
+            buttonRect.anchoredPosition = new Vector2(0, -i * (buttonHeight + buttonSpacing));
+
+            Text buttonText = pathButton.GetComponentInChildren<Text>();
+            if (buttonText != null)
+            {
+                buttonText.text = $"Путь {pathIndex + 1}";
+            }
+
+            pathButton.onClick.AddListener(() => OnPathButtonClicked(pathIndex));
+
+            pathButtons.Add(pathButton);
+        }
+
+        hireTeamButton.onClick.AddListener(OnHireTeamClicked);
+        // buyGoodsButton.onClick.AddListener(OnBuyGoodsClicked);
+    }
+
+    public void OpenPanel(City city)
     {
         currentCity = city;
-
-        // Обновляем UI города
-        cityNameText.text = city.cityName;
-        panel.SetActive(true);
-
-        Debug.Log($"Открыта панель города: {city.cityName}");
-    }
-
-    private void OpenTrade()
-    {
-        if (currentCity != null)
-        {
-            tradeSystem.OpenCityTrade(currentCity);
-        }
-        else
-        {
-            Debug.LogError("Не выбран текущий город!");
-        }
+        InitializePanel();
+        gameObject.SetActive(true);
     }
 
     public void ClosePanel()
     {
-        panel.SetActive(false);
-        tradeSystem.CloseTrade(); // Закрываем и торговую панель
+        gameObject.SetActive(false);
+    }
+
+    private void OnPathButtonClicked(int pathIndex)
+    {
+        Debug.Log($"Выбран путь {pathIndex + 1} в городе {currentCity.CityName}");
+        playerToken.SetPath(currentCity.Paths[pathIndex]); // Устанавливаем путь для фишки
+        ClosePanel(); // Закрываем панель после выбора пути
+    }
+
+    private void OnHireTeamClicked()
+    {
+        Debug.Log($"Нажата кнопка найма команды в городе {currentCity.CityName}");
+    }
+
+    /* private void OnBuyGoodsClicked()
+     {
+         if (tradeSystem != null)
+         {
+             tradeSystem.OpenTradePanel(); // Открываем панель торговли
+         }
+         else
+         {
+             Debug.LogError("TradeSystem не найден!");
+         }
+         Debug.Log($"Нажата кнопка покупки товаров в городе {currentCity.CityName}");
+     }*/
+
+    private void ClearPathButtons()
+    {
+        foreach (var button in pathButtons)
+        {
+            if (button != null)
+            {
+                button.onClick.RemoveAllListeners();
+                Destroy(button.gameObject);
+            }
+        }
+        pathButtons.Clear();
     }
 }
